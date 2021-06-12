359e9b23e7aeca4e6b741df189cb62fbe17c1b8c
Ainars Ciesa <ainars.c@gmail.com>
2021-06-12 15:21:24 -0600

    Add boilerplate theming

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,14 +4,16 @@ import GlobalStyles from "./globalStyles"
 import PropTypes from "prop-types"
 import NavBar from "./navBar"
 import Footer from "./footer"
+import theme from './theme'
 
 
 const Layout = ({ children }) => {
   return (
     <>
-    <GlobalStyles />
+    
     {/* <NavBar /> */}
-    <Main>{children}</Main>
+    <main>
+    <GlobalStyles theme={theme}/>{children}</main>
     {/* <Footer /> */}
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,9 @@ import Layout from '../components/layout'
 // markup
 const IndexPage = () => {
   return (
-    <main>
       <Layout>
         <div>FOO BAR</div>
       </Layout>
-      
-    </main>
   )
 }
 

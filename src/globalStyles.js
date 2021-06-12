e5f7a2cddfafdb923484faca4e50323247deba2e
Ainars Ciesa <ainars.c@gmail.com>
2021-06-12 14:29:12 -0600

    Add global styles and theme

---
+++ src/globalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    font-family: 'raleway', sans-serif; 
+    text-decoration: none;
+    box-sizing: border-box;
+    color: ${({ theme }) => theme.primaryLight};
+    font-size: 16px;
+    letter-spacing: 1px;
+    -webkit-tap-highlight-color: transparent;
+    font-weight: 300;
+    margin: 0;
+    text-decoration: none;
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+    background-color: ${({ theme }) => theme.primaryDark};
+  }
+ 
+`
+
+export default GlobalStyles

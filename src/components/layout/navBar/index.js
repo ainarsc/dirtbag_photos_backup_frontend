fc4864804a3bb6468b0d81509c796f2c934e48bd
Ainars <ainars.c@gmail.com>
2021-07-24 13:23:57 -0600

    Clean up

--- src/components/layout/navBar/index.js
+++ src/components/layout/navBar/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "gatsby"
 import styled from 'styled-components'
 import MenuIcon from './menuIcon'
 import SideBar from './menu'
@@ -8,36 +7,11 @@ import SideBar from './menu'
 
 export default () => {
     //Brand section
-    const BRAND = 'AC PHOTO',
-    //Link names
-     NAV_LINK_NAME_1 = 'Home',
-     NAV_LINK_NAME_2 = 'Portfolio',
-     NAV_LINK_NAME_3 = 'About',
-     NAV_LINK_NAME_4 = 'Contact',
-     //Links
-     LINK_1 = '/',
-     LINK_2 = '/portfolio',
-     LINK_3 = '/about',
-     LINK_4 = '/contact'
-
+    const BRAND = 'AC PHOTO'
 
     return (
         <Wrapper>
             <Brand>{BRAND}</Brand> 
-            {/* <StyledList>
-                <ListItem>
-                    <StyledLink exact activeClassName="active" to={LINK_1}>{NAV_LINK_NAME_1}</StyledLink>
-                </ListItem>
-                <ListItem>
-                    <StyledLink activeClassName="active" to={LINK_2}>{NAV_LINK_NAME_2}</StyledLink>
-                </ListItem>
-                <ListItem>
-                    <StyledLink activeClassName="active" to={LINK_3}>{NAV_LINK_NAME_3}</StyledLink>
-                </ListItem>
-                <ListItem>
-                    <StyledLink activeClassName="active" to={LINK_4}>{NAV_LINK_NAME_4}</StyledLink>
-                </ListItem>
-            </StyledList> */}
             <MenuIcon/>
             <SideBar/>
         </Wrapper>
@@ -51,44 +25,7 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: space-around;
 `
-const StyledList = styled.ul`
-    padding: 0;
-    margin: 0;
-    display: flex;
-    flex-direction: row;
-    flex: 3;
-    text-align: center;
-    align-items: center;
-    justify-content: space-around;
-    list-style: none; 
-`
-const ListItem = styled.li`
-    & a {
-        font-family: 'Zen Tokyo Zoo', cursive;
-        color: #dfe6e9;
-        text-transform: uppercase;
-        font-weight: 300;
-        margin: 0 1.5rem;
-        transition: all 300ms linear 0s;
-        text-decoration: none;
-        cursor: pointer;
-        &.active {
-        color: ${({ theme }) => theme.themeColors.alternative}};
-    }
-    
-    
-`
-const StyledLink = styled(Link)`
-    color: ${({ theme }) => theme.themeColors.alternative};
-    text-decoration: none;
-    font-weight: 600;
-    padding: 5px;
-    width: 100%;
-    &:hover {
-      color: ${({ theme }) => theme.themeColors.alternative};
-    }
-    
-`
+
 const Brand = styled.div`
     display: flex;
     align-items: center;
